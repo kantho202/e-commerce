28b92751949790fa529c6737eee14cd116208f04
kantho202 <kanthochakma2025@gmail.com>
2026-03-22 16:25:58 +0600

    navbar and footer setup

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/layouts/Navbar";
+import Footer from "@/layouts/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,17 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <header>
+          <Navbar></Navbar>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>
+          <Footer></Footer>
+        </footer>
+        </body>
     </html>
   );
 }
