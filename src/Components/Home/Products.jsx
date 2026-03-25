7c67b6e8c8f532ea9b5fa42fc89b03c0178d331e
kantho202 <kanthochakma2025@gmail.com>
2026-03-25 15:52:05 +0600

    products description update

--- src/Components/Home/Products.jsx
+++ src/Components/Home/Products.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Products = () => {
     return (
         <div>
-            Products
+        <h1>Our Products</h1>
+               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae reiciendis 
+                similique delectus inventore aut aliquam accusantium numquam consequuntur maxime
+                 necessitatibus optio, quaerat iusto rerum magni accusamus quis ab et unde?</p>
         </div>
     );
 };
