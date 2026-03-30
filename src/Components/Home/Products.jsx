ddbe5fb774309444d5f83705686efa96566d886d
kantho202 <kanthochakma2025@gmail.com>
2026-03-31 00:50:28 +0600

    hook create

--- src/Components/Home/Products.jsx
+++ src/Components/Home/Products.jsx
@@ -1,12 +1,27 @@
+"use client"
+import useAxiosSecure from '@/hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const Products = () => {
+    const axiosSecure = useAxiosSecure()
+    const { data: products = [] } = useQuery({
+        // queryKey: ['products'],
+        // queryFn: async () => {
+        //     const res = await axiosSecure.get("/products")
+        //     res.data;
+        // }
+        queryFn: () =>
+      fetch('http://localhost:5000/api/v1/products').then((res) =>
+        res.json(),
+      ),
+    })
     return (
         <div>
-        <h1>Our Products</h1>
-               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae reiciendis 
+            <h1>Our Products <span>{products.length}</span> </h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae reiciendis
                 similique delectus inventore aut aliquam accusantium numquam consequuntur maxime
-                 necessitatibus optio, quaerat usamus quis ab et unde?</p>
+                necessitatibus optio, quaerat usamus quis ab et unde?</p>
         </div>
     );
 };
