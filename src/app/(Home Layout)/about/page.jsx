e2ec6ff55ea675707af60e7a1ecff2f3abccbbd4
kantho202 <kanthochakma2025@gmail.com>
2026-04-03 13:18:17 +0600

    about page to added paragraph

--- src/app/(Home Layout)/about/page.jsx
+++ src/app/(Home Layout)/about/page.jsx
@@ -4,6 +4,7 @@ const page = () => {
     return (
         <div>
             <h1>This is a about page </h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum deleniti, aut nesciunt dolorum animi corrupti molestiae ut vero. Est incidunt amet, eaque id quaerat reiciendis voluptas dolorum unde repellendus cumque!</p>
         </div>
     );
 };
