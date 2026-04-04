c5bef2e2da5db4f4539cabd73c7bb8f29e9a785d
kantho202 <kanthochakma2025@gmail.com>
2026-04-04 21:38:04 +0600

    login page desgin

--- src/app/(Home Layout)/login/page.jsx
+++ src/app/(Home Layout)/login/page.jsx
@@ -4,6 +4,17 @@ const page = () => {
     return (
         <div>
             <h1>This is log in page</h1>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+  <legend className="fieldset-legend">Login</legend>
+
+  <label className="label">Email</label>
+  <input type="email" className="input" placeholder="Email" />
+
+  <label className="label">Password</label>
+  <input type="password" className="input" placeholder="Password" />
+
+  <button className="btn btn-neutral mt-4">Login</button>
+</fieldset>
         </div>
     );
 };
