c1fb8211dc709785eea6ac1b9802e7d11369d9fa
kantho202 <kanthochakma2025@gmail.com>
2026-04-05 23:14:02 +0600

    contact page design

--- src/app/(Home Layout)/about/page.jsx
+++ src/app/(Home Layout)/about/page.jsx
@@ -5,6 +5,7 @@ const page = () => {
         <div>
             <h1>This is a about page </h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum deleniti, aut nesciunt dolorum animi corrupti molestiae ut vero. Est incidunt amet, eaque id quaerat reiciendis voluptas dolorum unde repellendus cumque!</p>
+            
         </div>
     );
 };
