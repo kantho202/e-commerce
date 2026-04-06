0b1467ff450742319c8c5591184df61e7cb89671
kantho202 <kanthochakma2025@gmail.com>
2026-04-06 16:03:15 +0600

    contact page description

--- src/app/(Home Layout)/Contact/page.jsx
+++ src/app/(Home Layout)/Contact/page.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            This  is a contact page 
+            <h1>This  is a contact page </h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates delectus voluptas rem mollitia fuga aspernatur voluptate assumenda quod at magnam saepe numquam voluptatibus laborum quo eum, esse culpa? Nobis, nemo!</p>
         </div>
     );
 };
